24a513d199f7e8a3f9e09b6188d85d09030e8944
Shumaila Malik <shumaila.malik@cofinity-x.com>
2025-04-16 10:01:21 +0200

    feat: CS-3158 --Enhance the logging of Data Deletion Job (#100)
    Description
    The Data Deletion Job is responsible for delete and notify the portal credentials that are about to expire. With the current log file it is not possible to identify if the service deleted or notified the Portal regarding any credential. It is possible to know only that the Job has run well.

--- src/credentials/SsiCredentialIssuer.DataDeletion.App/DataDeletionCheckService.cs
+++ src/credentials/SsiCredentialIssuer.DataDeletion.App/DataDeletionCheckService.cs
@@ -86,10 +86,12 @@ public class ExpiryCheckService
 
                 var credentials = outerLoopRepositories.GetInstance<ICompanySsiDetailsRepository>()
                     .GetExpiryData(now, inactiveVcsToDelete, expiredVcsToDelete);
+                _logger.LogInformation("Total number of credentials to be processed for details are {Count}", await credentials.CountAsync(stoppingToken).ConfigureAwait(false));
                 await foreach (var credential in credentials.WithCancellation(stoppingToken).ConfigureAwait(false))
                 {
                     await ProcessCredentials(credential, companySsiDetailsRepository, repositories, portalService, processStepRepository, stoppingToken).ConfigureAwait(ConfigureAwaitOptions.None);
                 }
+                _logger.LogInformation("Credential details process completed successfully");
             }
             catch (Exception ex)
             {
@@ -107,21 +109,30 @@ public class ExpiryCheckService
         IProcessStepRepository processStepRepository,
         CancellationToken cancellationToken)
     {
-        if (data.ScheduleData.IsVcToDelete)
+        try
         {
-            companySsiDetailsRepository.RemoveSsiDetail(data.Id, data.Bpnl, data.RequesterId);
-        }
-        else if (data.ScheduleData.IsVcToDecline)
-        {
-            HandleDecline(data.Id, companySsiDetailsRepository, processStepRepository);
-        }
-        else
-        {
-            await HandleNotification(data, companySsiDetailsRepository, portalService, cancellationToken).ConfigureAwait(false);
-        }
+            if (data.ScheduleData.IsVcToDelete)
+            {
+                companySsiDetailsRepository.RemoveSsiDetail(data.Id, data.Bpnl, data.RequesterId);
+            }
+            else if (data.ScheduleData.IsVcToDecline)
+            {
+                HandleDecline(data.Id, companySsiDetailsRepository, processStepRepository);
+            }
+            else
+            {
+                await HandleNotification(data, companySsiDetailsRepository, portalService, cancellationToken).ConfigureAwait(false);
+            }
 
-        // Saving here to make sure the each credential is handled by there own 
-        await repositories.SaveAsync().ConfigureAwait(ConfigureAwaitOptions.None);
+            // Saving here to make sure the each credential is handled by there own 
+            await repositories.SaveAsync().ConfigureAwait(ConfigureAwaitOptions.None);
+
+        }
+        catch (Exception ex)
+        {
+            var message = $"An error occurred while processing credential details '{ex.Message}' for ID '{data.Id}'";
+            throw new InvalidOperationException(message, ex);
+        }
     }
 
     private static void HandleDecline(
